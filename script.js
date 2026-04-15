9626377f6e61633b258b83bd9379c4a54300efc3
YEN20622 <student10618@gmail.com>
2026-04-15 13:54:58 +0800

    Remove sound toggle function
    Remove sound toggle functionality from script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-<script>
-function toggleSound() {
-  const video = document.getElementById("bg-video");
-  const btn = document.getElementById("sound-btn");
 
-  if (video.muted) {
-    video.muted = false;
-    video.volume = 1.0; // 音量
-    btn.innerText = "🔊 關閉聲音";
-  } else {
-    video.muted = true;
-    btn.innerText = "🔇 開啟聲音";
-  }
-}
-</script>
